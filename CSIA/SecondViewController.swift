bc248ab980d98e47b60ad0829e5ffeec96091185
bernicetse <lingtbt2021@student.cis.edu.hk>
2020-08-16 11:57:49 +0800

    Initial Commit

---
+++ CSIA/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CSIA
+//
+//  Created by Bernice Tse on 16/8/2020.
+//  Copyright © 2020 Bernice Tse. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
